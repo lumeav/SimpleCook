4683decacd5648daa0a81ccecfff59ecda7c6cdf
dennishoang <dennishoang199@gmail.com>
2024-06-28 14:05:57 +0200

    resolve merge conflic recipefinder

--- simple_cook/lib/ui/recipefinder/recipefinder_view.dart
+++ simple_cook/lib/ui/recipefinder/recipefinder_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:simple_cook/service/single_recipe_model.dart';
-=======
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:simple_cook/common/custom_navbar.dart';
 import 'package:simple_cook/ui/recipeFinder/recipefinder_controller_implementation.dart';
->>>>>>> feature_riverpod
 import 'package:simple_cook/widgets/simple_cook_appbar.dart';
 import 'package:simple_cook/widgets/filter_tag.dart';
 import 'package:simple_cook/widgets/slider_filter.dart';
@@ -15,13 +12,9 @@ import 'package:simple_cook/widgets/search_recipe_button.dart';
 import 'package:simple_cook/widgets/header_grey_background.dart';
 import 'package:simple_cook/service/recipe_service/recipe_service.dart';
 
-<<<<<<< HEAD
-class RecipefinderView extends StatefulWidget {
-=======
 class RecipefinderView extends ConsumerStatefulWidget {
 
 
->>>>>>> feature_riverpod
   RecipefinderView({
     Key? key,
   }) : super(key: key);
@@ -43,33 +36,6 @@ class _RecipefinderViewState extends ConsumerState<RecipefinderView> {
             radius: const Radius.circular(50),
             thickness: 5,
             child: SingleChildScrollView(
-<<<<<<< HEAD
-              physics: const ClampingScrollPhysics(),
-              child: Column(
-                children: [
-                  SearchBarFilter(),
-                  const HeaderGreyBackground("Kategorie", FontWeight.bold),
-                  _buildFilterTags(kategorieList),
-                  const HeaderGreyBackground("Ernährungsart",
-                      FontWeight.bold), // HeaderGreyBackground("Ernährungsart"
-                  _buildFilterTags(ernaehrungsartList),
-                  const HeaderGreyBackground(
-                      "Zubereitungszeit", FontWeight.bold),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15),
-                    child: SliderFilter(),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        loadRecipes();
-                      },
-                      child: const Text('Rezepte suchen')),
-                  Container(
-                      margin: const EdgeInsets.only(top: 50, bottom: 30),
-                      alignment: Alignment.center,
-                      child: SearchRecipesButton(
-                          "Rezepte suchen", "subRecipesFiltered")),
-=======
               physics:  ClampingScrollPhysics(),
               child: Column(
                 children: [
@@ -89,7 +55,6 @@ class _RecipefinderViewState extends ConsumerState<RecipefinderView> {
                     child: const SearchRecipesButton("Rezept generieren", "subRecipesFiltered")
 
                   )
->>>>>>> feature_riverpod
                 ],
               ),
             ),
@@ -97,35 +62,6 @@ class _RecipefinderViewState extends ConsumerState<RecipefinderView> {
         ));
   }
 
-<<<<<<< HEAD
-  Future<void> loadRecipes() async {
-    final RecipeService recipeService = RecipeService();
-    final SingleRecipe singlerecipe = await recipeService.getSingleRecipe(
-        'https://www.lecker.de/deftiger-kuechenzauber-topf-83483.html'); //getAllRecipes('Tomate', null, null);
-    for (var recipe in singlerecipe!.ingredients!) {
-      print(recipe);
-    }
-  }
-
-  List<String> kategorieList = [
-    'Alle',
-    'Vorspeise',
-    'Hauptspeise',
-    'Dessert',
-    'Snacks',
-  ];
-
-  List<String> ernaehrungsartList = [
-    'Vegetarisch',
-    'Vegan',
-    'Glutenfrei',
-    'Laktosefrei',
-    'Low Carb',
-  ];
-  //maybe refactor methods to widgets
-
-=======
->>>>>>> feature_riverpod
   Widget _buildFilterTags(List<String> filterList) {
     return Align(
       alignment: Alignment.centerLeft,
@@ -146,8 +82,6 @@ class _RecipefinderViewState extends ConsumerState<RecipefinderView> {
     );
   }
 }
-<<<<<<< HEAD
-=======
 
 abstract class RecipeFinderController {
   void setFilterActive(String filter);
@@ -156,4 +90,3 @@ abstract class RecipeFinderController {
 }
 
 
->>>>>>> feature_riverpod
