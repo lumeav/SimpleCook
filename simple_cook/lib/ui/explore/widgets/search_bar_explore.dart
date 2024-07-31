5da286a311e44790209e88e1e2c0fba3e2cf6d4b
lumeav <Lukas.Mertke@htwg-konstanz.de>
2024-07-31 20:02:32 +0200

    final cleanup

--- simple_cook/lib/ui/explore/widgets/search_bar_explore.dart
+++ simple_cook/lib/ui/explore/widgets/search_bar_explore.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:simple_cook/common/theme.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SearchBarExplore extends StatefulWidget {
   const SearchBarExplore({super.key});
